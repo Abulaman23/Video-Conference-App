b7d862855d265cdc6c8f30bfa6da8b2241afcba5
KenilViradiya <129634846+KenilViradiya@users.noreply.github.com>
2024-08-12 18:01:16 +0530

    Add Contact Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:video_conference_app/screens/AddContactsScreen.dart';
+import 'package:video_conference_app/screens/home_screen.dart';
+import 'package:video_conference_app/screens/onboardingscreens/onboarding_screen_1.dart';
+import 'package:video_conference_app/screens/signup_screen_2.dart';
 import 'package:video_conference_app/screens/splash_screen.dart';
 import 'package:video_conference_app/utils/themes.dart';
 //import 'package:video_conference_app/screens/splash_screen.dart';
@@ -18,7 +22,7 @@ class MyApp extends StatelessWidget {
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      home:  HomeScreen(),
     );
   }
 }
